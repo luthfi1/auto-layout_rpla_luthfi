167e6db9f45d19a328798af89baeb1f6700e79ae
luthfi1 <68719137+luthfi1@users.noreply.github.com>
2020-11-17 12:44:19 +0700

    Add files via upload

---
+++ apk boal rpl a/apk boal rpl a/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  apk boal rpl a
+//
+//  Created by muhammad luthfi farizqi on 10/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ aplikasi bola/aplikasi bola/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  aplikasi bola
+//
+//  Created by muhammad luthfi farizqi on 10/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Imange1: UIImageView!
+    override func viewDidLoad() {
+        @IBOutlet weak var image2: UIImageView!
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
